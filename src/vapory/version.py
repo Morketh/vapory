5523b862e084bd225af9e4d95a10bc282fcc3acf
Andrew Malone <admmorketh@gmail.com>
2026-06-26 20:07:14 -0600

    fix(version): updates version string for actual package metadata

--- src/vapory/version.py
+++ src/vapory/version.py
@@ -1,6 +1,6 @@
 from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = version("collective-cores")
+    __version__ = version("Vapory")
 except PackageNotFoundError:
     __version__ = "unknown"
